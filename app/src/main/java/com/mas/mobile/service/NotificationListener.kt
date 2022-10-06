c4023df87a3b8968d470c4422831d82781b01327
Andrii Artemenko <andrii.artemenko@kingfisher.com>
2022-10-06 17:20:22 +0200

    feat: add revolut support

--- app/src/main/java/com/mas/mobile/service/NotificationListener.kt
+++ app/src/main/java/com/mas/mobile/service/NotificationListener.kt
@@ -1,10 +1,12 @@
 package com.mas.mobile.service
 
+import android.app.Notification
 import android.app.Notification.EXTRA_TEXT
 import android.app.Notification.EXTRA_TITLE
 import android.content.pm.PackageManager
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.util.Log
 import com.mas.mobile.appComponent
 import java.time.Instant
 import java.time.LocalDateTime
@@ -29,9 +31,41 @@ class NotificationListener: NotificationListenerService() {
             val sender = extractSender(sbn).toString()
             val title = sbn.notification.extras[EXTRA_TITLE]?.toString()
             val text = sbn.notification.extras[EXTRA_TEXT]?.toString()
-            val message = "$title\n$text"
+
+            val message = if (title == null && text == null) {
+                grabCustomContentView(sbn.notification)
+            } else {
+                "$title\n$text"
+            }
+
             process(Message(sender, postDate, message))
         }
+    }
+
+    private fun grabCustomContentView(notification: Notification) =
+        try {
+            val actions = getPrivateField<List<*>>("mActions", notification.contentView)
+            actions?.filterNotNull()
+                ?.filter { getPrivateField<String>("methodName", it) == "setText" }
+                ?.map { getPrivateField<String>("value", it) }
+                ?.joinToString("\n")
+        } catch (e: Throwable) {
+            Log.e(this::class.simpleName, "Can't grab notification from custom View", e)
+            null
+        } ?: ""
+
+    private fun <T> getPrivateField(name: String, instance: Any): T?  {
+        var clazz: Class<Any>? = instance.javaClass
+        do {
+            if (clazz!!.declaredFields.any { it.name == name }) {
+                break
+            }
+            clazz = clazz.superclass
+        } while (clazz != null)
+
+        return clazz?.getDeclaredField(name)
+            ?.also { it.isAccessible = true }
+            ?.get(instance) as T
     }
 
     private fun extractSender(sbn: StatusBarNotification) =
